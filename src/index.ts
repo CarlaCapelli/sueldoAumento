6008a96fa3c4315b31abae781dc6df5a74c75a9e
CarlaCapelli <92063946+CarlaCapelli@users.noreply.github.com>
2021-10-22 17:23:47 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,13 @@ btn.addEventListener("click", () => {
   let sueld: number = Number(sueldo.value);
   if (0 < sueld && sueld <= 15000) {
     sueldoaumento = sueld * 1.2;
-    console.log("tiene un aumento del 20%, su sueldo es de", sueldoaumento);
+    console.log("tiene un aumento del 20%, su sueldo es de "+ sueldoaumento);
   } else if (15000 < sueld && sueld <= 20000) {
     sueldoaumento = sueld * 1.1;
-    console.log("tiene un aumento del 10%, su sueldo es de", sueldoaumento);
+    console.log("tiene un aumento del 10%, su sueldo es de "+ sueldoaumento);
   } else if (20000 < sueld && sueld <= 25000) {
     sueldoaumento = sueld * 1.05;
-    console.log("tiene un aumento del 5%, su sueldo es de", sueldoaumento);
+    console.log("tiene un aumento del 5%, su sueldo es de "+ sueldoaumento);
   } else if (sueld > 25000) {
     sueldoaumento = sueld;
     console.log("no tiene aumento");
